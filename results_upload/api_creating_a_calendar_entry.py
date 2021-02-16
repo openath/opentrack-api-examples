a472bc5fb614a89411ce6f6d424c3a196c66b5a2
BenjaminJH <benjhassell@gmail.com>
2021-02-16 13:04:24 +0000

    Updated example to use GBR data

--- results_upload/api_creating_a_calendar_entry.py
+++ results_upload/api_creating_a_calendar_entry.py
@@ -14,25 +14,22 @@ token = resp["access_token"]
 headers = {'Authorization': 'Bearer %s' % token}
 
 comp = {
-    'full_name': 'api-test long name',
-    'short_name': 'api-test',
-    'slug': 'api-test-slug',
-    'name_local': 'api-test in italiano',
-    'country_id': 'ITA',
-    'address': 'Via dello Stadio 1, City',
-    'latitude': '20.123',
-    'longitude': '-15.321',
-    'altitude': '1200.14',
-    'date': '2020-08-10',
-    'finish_date': '2020-08-15',
-    'wa_rankings_category_id': 'DF',
-    'age_groups': ['ALL'],
-    'basic_description': 'Competition in Italy',
-    'contact_details': 'example@example.it',
-    'organiser_id': '179c0872-f761-4ed1-bb75-3b58a1368bac',
-    'website': 'https://example.example',
-    'entry_link': 'https://example.example/entry',
-    'results_link': 'https://example.example/results',
+  "full_name": "api-test open meeting",
+  "short_name": "api-test",
+  "slug": "api-test-slug",
+  "name_local": "api-test",
+  "country_id": "GBR",
+  "address": "Kingsmeadow, Jack Goodchild Way, Kingston Road, Kingston upon Thames, UK",
+  "date": "2020-08-10",
+  "finish_date": "2020-08-15",
+  "wa_rankings_category_id": "DF",
+  "age_groups": ["ALL"],
+  "basic_description": "This is an opentrack api test",
+  "contact_details": "example@example.com",
+  "organiser_id": "6b2af700-0481-4f73-b9ae-8221ae619b55",
+  "website": "https://example.example",
+  "entry_link": "https://example.example/entry",
+  "results_link": "https://example.example/results"
 }
 
 
