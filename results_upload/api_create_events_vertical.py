8c4ef7c35e4b95fe566c6759f15d184db13a550d
BenjaminJH <benjhassell@gmail.com>
2021-02-16 11:45:45 +0000

    Updated vertical event code

--- results_upload/api_create_events_vertical.py
+++ results_upload/api_create_events_vertical.py
@@ -14,25 +14,60 @@ token = resp["access_token"]
 headers = {'Authorization': 'Bearer %s' % token}
 
 comp = {
-    "full_name": "api-test long name events vertical",
+    "full_name": "api-test events vertical",
     "short_name": "api-test",
     "slug": "api-test-slug-2c",
-    "name_local": "api-test in italiano",
-    "country_id": "ITA",
-    "address": "Via dello Stadio 1, City",
-    "latitude": "20.123",
-    "longitude": "-15.321",
-    "altitude": "1200.14",
+    "name_local": "api-test",
+    "country_id": "GBR",
+    "address": "Kingsmeadow, Jack Goodchild Way, Kingston Road, Kingston upon Thames, UK",
     "date": "2020-08-10",
     "finish_date": "2020-08-15",
     "wa_rankings_category_id": "DF",
     "age_groups": ["ALL"],
-    "basic_description": "Competition in Italy",
-    "contact_details": "example@example.it",
-    "organiser_id": "179c0872-f761-4ed1-bb75-3b58a1368bac",
+    "basic_description": "This is an opentrack api test",
+    "contact_details": "example@example.com",
+    "organiser_id": "6b2af700-0481-4f73-b9ae-8221ae619b55",
     "website": "https://example.example",
     "entry_link": "https://example.example/entry",
     "results_link": "https://example.example/results",
+    "competitors": [
+        {
+            "age_group": "NA", 
+            "category": "NA",
+            "competitor_id": "371", 
+            "date_of_birth": "1966-03-21",
+            "events_entered": [
+                {
+                    "event_code": "PV", 
+                    "event_id": "F2"
+                }
+            ], 
+            "first_name": "Andrew",
+            "gender": "M", 
+            "last_name": "Robinson", 
+            "nationality": "GBR",
+            "ot_athlete_id": "44721dcd-3019-44f0-aab0-4b90eb98838d", 
+            "team_id": "THH"
+        },
+        {
+            "age_group": "NA", 
+            "category": "NA",
+            "competitor_id": "302", 
+            "date_of_birth": "1989-04-26",
+            "events_entered": [
+                {
+                    "event_code": "PV", 
+                    "event_id": "1"
+                }
+            ], 
+            "first_name": "Gus",
+            "gender": "M", 
+            "last_name": "Upton", 
+            "nationality": "GBR",
+            "ot_athlete_id": "af0a1892-0c9d-4031-a4cf-690c3c37bc49", 
+            "team_id": "BEL"
+        }
+    ],
     "events": [
         {
             "age_groups": ["SEN"],
