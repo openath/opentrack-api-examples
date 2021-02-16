ff1f8564361edbca6ea26e5e9bc8ca7a62bebbf4
BenjaminJH <benjhassell@gmail.com>
2021-02-16 12:47:25 +0000

    updated running event file

--- results_upload/api_create_events_running.py
+++ results_upload/api_create_events_running.py
@@ -1,8 +1,8 @@
 import requests
 import json
 
-EMAIL = '<put your email here>'
-PASSWORD = '<put your password here>'
+EMAIL = '<Your authorised Email>'
+PASSWORD = '<Your authorised password>'
 
 r = requests.post(
     "https://test-api.opentrack.run/api/v1/auth/token", 
@@ -14,26 +14,78 @@ token = resp["access_token"]
 headers = {'Authorization': 'Bearer %s' % token}
 
 comp = {
-    "full_name": "api-test long name events",
+    "full_name": "api-test events",
     "short_name": "api-test",
-    "slug": "api-test-slug-2",
-    "name_local": "api-test in italiano",
-    "country_id": "ITA",
-    "address": "Via dello Stadio 1, City",
-    "venue_id": "051e8072-f667-4f10-aa82-b16a9317588e",
-    "latitude": "20.123",
-    "longitude": "-15.321",
-    "altitude": "1200.14",
+    "slug": "api-test-slug-2a",
+    "name_local": "api-test",
+    "country_id": "GBR",
+    "address": "Kingsmeadow, Jack Goodchild Way, Kingston Road, Kingston upon Thames, UK",
     "date": "2020-08-10",
     "finish_date": "2020-08-15",
     "wa_rankings_category_id": "DF",
     "age_groups": ["ALL"],
-    "basic_description": "Competition in Italy",
-    "contact_details": "example@example.it",
-    "organiser_id": "179c0872-f761-4ed1-bb75-3b58a1368bac",
+    "basic_description": "This is an opentrack api test",
+    "contact_details": "example@example.com",
+    "organiser_id": "6b2af700-0481-4f73-b9ae-8221ae619b55",
     "website": "https://example.example",
     "entry_link": "https://example.example/entry",
     "results_link": "https://example.example/results",
+    "competitors": [
+        {
+            "age_group": "NA", 
+            "category": "NA",
+            "competitor_id": "1", 
+            "date_of_birth": "1966-03-21",
+            "events_entered": [
+                {
+                    "event_code": "3k", 
+                    "event_id": "1"
+                }
+            ], 
+            "first_name": "Andrew",
+            "gender": "M", 
+            "last_name": "Robinson", 
+            "nationality": "GBR",
+            "ot_athlete_id": "44721dcd-3019-44f0-aab0-4b90eb98838d", 
+            "team_id": "THH"
+        },
+        {
+            "age_group": "NA", 
+            "category": "NA",
+            "competitor_id": "2", 
+            "date_of_birth": "1967-08-22",
+            "events_entered": [
+                {
+                    "event_code": "3k", 
+                    "event_id": "1"
+                }
+            ], 
+            "first_name": "Andy",
+            "gender": "M", 
+            "last_name": "Weir", 
+            "nationality": "GBR",
+            "ot_athlete_id": "332b0c3e-da43-4be6-af18-20a70fb23912", 
+            "team_id": "THH"
+        },
+        {
+            "age_group": "NA", 
+            "category": "NA",
+            "competitor_id": "3", 
+            "date_of_birth": "1989-04-26",
+            "events_entered": [
+                {
+                    "event_code": "3k", 
+                    "event_id": "1"
+                }
+            ], 
+            "first_name": "Gus",
+            "gender": "M", 
+            "last_name": "Upton", 
+            "nationality": "GBR",
+            "ot_athlete_id": "af0a1892-0c9d-4031-a4cf-690c3c37bc49", 
+            "team_id": "BEL"
+        }
+    ],
     "events": [
         {
             "age_groups": [
@@ -55,13 +107,13 @@ comp = {
                         {
                             "place": 1,
                             "points": 0,
-                            "bib": "ZZ000002",
+                            "bib": "1",
                             "performance": "10.37"
                         },
                         {
                             "place": 2,
                             "points": 0,
-                            "bib": "ZZ000011",
+                            "bib": "2",
                             "performance": "10.48"
                         }
                     ]
@@ -75,14 +127,8 @@ comp = {
                         {
                             "place": 1,
                             "points": 0,
-                            "bib": "ZZ000003",
+                            "bib": "3",
                             "performance": "10.26"
-                        },
-                        {
-                            "place": 2,
-                            "points": 0,
-                            "bib": "ZZ000004",
-                            "performance": "10.47"
                         }
                     ]
                 },
@@ -95,26 +141,20 @@ comp = {
                         {
                             "place": 1,
                             "points": 0,
-                            "bib": "ZZ000002",
+                            "bib": "1",
                             "performance": "10.07"
                         },
                         {
                             "place": 2,
                             "points": 0,
-                            "bib": "ZZ000003",
+                            "bib": "3",
                             "performance": "10.37"
                         },
                         {
                             "place": 3,
                             "points": 0,
-                            "bib": "ZZ000011",
+                            "bib": "2",
                             "performance": "10.47"
-                        },
-                        {
-                            "place": 4,
-                            "points": 0,
-                            "bib": "ZZ000004",
-                            "performance": "10.48"
                         }
                     ]
                 }
@@ -122,6 +162,7 @@ comp = {
         }
     ]
 }
+
 
 
 r = requests.post(
@@ -133,3 +174,4 @@ r = requests.post(
 print("Created competiton with status_code %d" %  r.status_code)
 parsed = json.loads(r.text)
 print(json.dumps(parsed, indent=4, sort_keys=False))
+
